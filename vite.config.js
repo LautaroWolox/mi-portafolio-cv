4beea1306f1700b687d64c881f548a1650147769
laucha <mlautarobuson@gmail.com>
2025-11-23 01:35:04 -0300

    Fix: Asegurando la ruta base en vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// Esta configuración asegura que las rutas de los assets sean correctas para Netlify
 export default defineConfig({
   plugins: [react()],
+  // Definimos la ruta base como la raíz.
+  base: '/',
 })
